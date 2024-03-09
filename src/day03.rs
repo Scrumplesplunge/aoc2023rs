5ff8d313fa513632eac9d47312555d408b147b0b
Joe Fowler <scrumplesplunge@gmail.com>
2024-03-09 21:51:32 +0000

    Rewrite day 3 with iterators for parts and neighbours.

--- src/day03.rs
+++ src/day03.rs
@@ -20,96 +20,92 @@ fn read_input() -> Grid {
     return grid;
 }
 
-fn part1(grid: &Grid) -> u32 {
-    // Create a second grid which expands influence outwards from symbols.
-    let mut influence = [[false; 140]; 140];
-    for y in 0..140 {
-        for x in 0..140 {
-            let c = grid[y as usize][x as usize];
-            if c != '.' && !c.is_digit(10) {
-                for iy in y - 1..y + 2 {
-                    if iy < 0 || 140 <= iy { continue }
-                    for ix in x - 1..x + 2 {
-                        if ix < 0 || 140 <= ix { continue }
-                        influence[iy as usize][ix as usize] = true;
-                    }
-                }
-            }
-        }
-    }
-    // Parse and sum all part numbers.
-    let mut total = 0;
-    for y in 0..140 {
-        let mut is_part = false;
-        let mut number = 0;
-        for x in 0..140 {
-            if let Some(d) = grid[y][x].to_digit(10) {
-                number = 10 * number + d;
-                is_part |= influence[y][x];
-            } else {
-                if is_part { total += number }
-                is_part = false;
-                number = 0;
-            }
-        }
-        if is_part { total += number }
-    }
-    return total;
+// NumberIterator iterates over numbers that appear in a sequence of characters.
+struct NumberIterator<'a, T: Iterator<Item = &'a char>> {
+    data: T,
 }
 
-fn gear_ratio(grid: &Grid, x: i32, y: i32) -> Option<u32> {
-    let mut count = 0;
-    let mut product = 1;
-    // Iterate over all positions that can contain adjacent numbers.
-    for iy in y - 1..y + 2 {
-        if iy < 0 || 140 <= iy { continue }
-        let row = &grid[iy as usize];
+impl<'a, T: Iterator<Item = &'a char>> Iterator for NumberIterator<'a, T> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Skip any non-digit characters.
+        let i = &mut self.data;
+        if let Some(c) = i.find(|c| c.is_digit(10)) {
+            let mut n = c.to_digit(10).unwrap();
+            while let Some(c) = i.next() {
+                if let Some(d) = c.to_digit(10) {
+                    n = 10 * n + d;
+                } else { break }
+            }
+            return Some(n);
+        } else {
+            return None;
+        }
+    }
+}
+
+fn numbers<'a, T: Iterator<Item = &'a char>>(i: T) -> NumberIterator<'a, T> {
+    return NumberIterator{data: i};
+}
+
+struct Part<'a> {
+    grid: &'a Grid,
+    x: usize,
+    y: usize,
+}
+
+fn to_part<'a>(grid: &'a Grid, x: usize, y: usize) -> Option<Part<'a>> {
+    let c = grid[y][x];
+    if c == '.' || c.is_digit(10) { return None }
+    return Some(Part{grid: &grid, x: x, y: y});
+}
+
+// Iterate over all parts in the grid.
+fn parts(grid: &Grid) -> impl Iterator<Item = Part> {
+    return (0..140).flat_map(move |x| {
+        (0..140).filter_map(move |y| {
+            to_part(grid, x, y)
+        })
+    });
+}
+
+// Iterate over all the numbers adjacent to a part.
+fn neighbours<'a>(part: &'a Part) -> impl Iterator<Item = u32> + 'a {
+    let min_y = if part.y == 0 { 0 } else { part.y - 1 };
+    let max_y = (part.y + 2).min(140);
+    return (min_y .. max_y).flat_map(move |iy| {
         // Scan left/right as far as there are digits. The individual numbers
         // are picked out by the loop below.
-        let min_x = row[..x as usize]
+        let row = &part.grid[iy];
+        let min_x = row[..part.x]
             .iter()
             .rposition(|c| !c.is_digit(10))
             .unwrap_or(0);
-        let max_x = row[x as usize + 1..]
+        let max_x = row[part.x + 1..]
             .iter()
             .position(|c| !c.is_digit(10))
-            .map(|i| x as usize + 1 + i)
+            .map(|i| part.x + 1 + i)
             .unwrap_or(140);
-        let mut number = None;
-        for ix in min_x..max_x {
-            if let Some(d) = grid[iy as usize][ix as usize].to_digit(10) {
-                number = Some(10 * number.unwrap_or(0) + d);
-            } else {
-                if let Some(n) = number {
-                    count += 1;
-                    product *= n;
-                    number = None;
-                    if count > 2 { return None }
-                }
-            }
-        }
-        if let Some(n) = number {
-            count += 1;
-            product *= n;
-            if count > 2 { return None }
-        }
-    }
-    if count == 2 { return Some(product) }
-    return None;
-}
-
-fn part2(grid: &Grid) -> u32 {
-    let mut total = 0;
-    for y in 0..140 {
-        for x in 0..140 {
-            if grid[y as usize][x as usize] != '*' { continue }
-            if let Some(x) = gear_ratio(grid, x, y) { total += x }
-        }
-    }
-    return total;
+        numbers(part.grid[iy][min_x .. max_x].iter())
+    });
 }
 
 fn main() {
     let grid = read_input();
-    print!("{}\n{}\n", part1(&grid), part2(&grid));
+    let mut part1 = 0;
+    let mut part2 = 0;
+    // Assumption #1: No number is adjacent to two symbols.
+    // Assumption #2: Only '*' symbols are adjacent to exactly two numbers.
+    for part in parts(&grid) {
+        let mut count = 0;
+        let mut product = 1;
+        for number in neighbours(&part) {
+            count += 1;
+            part1 += number;
+            product *= number;
+        }
+        if count == 2 { part2 += product }
+    }
+    print!("{}\n{}\n", part1, part2);
 }
