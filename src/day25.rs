d9468a1d1938940cc3db8eaf1a20284ec1dde071
Joe Fowler <scrumplesplunge@gmail.com>
2024-03-31 20:19:56 +0100

    Karger -> Karger-Stein: 2.5s -> 60ms

--- src/day25.rs
+++ src/day25.rs
@@ -46,15 +46,17 @@ fn read_input<'a>(edges: &'a mut[(u16, u16)]) -> (u16, &'a [(u16, u16)]) {
             }
         }
     }
-    return (next_id, &edges[0..num_edges]);
+    return (next_id, &mut edges[0..num_edges]);
 }
 
-fn karger(rng: &mut ThreadRng, mut num_nodes: u16, input: &[(u16, u16)]) -> (usize, u32, u32) {
-    let mut edge_buffer = [(0, 0); MAX_EDGES];
-    let mut edges = &mut edge_buffer[0..input.len()];
-    let mut size = [1; MAX_NODES];
-    edges.copy_from_slice(input);
-    while num_nodes > 2 {
+fn contract<'a>(
+    rng: &mut ThreadRng,
+    size: &mut [u32],
+    mut edges: &'a mut [(u16, u16)],
+    mut num_nodes: u16,
+    target_num_nodes: u16,
+) -> &'a mut [(u16, u16)] {
+    while num_nodes > target_num_nodes {
         // Pick a random edge.
         let i = Uniform::from(0..edges.len()).sample(rng);
         let (a, b) = edges[i];
@@ -74,22 +76,53 @@ fn karger(rng: &mut ThreadRng, mut num_nodes: u16, input: &[(u16, u16)]) -> (usi
         }
         edges = &mut edges[0..j];
     }
-    let cut_size = edges.len();
-    let (a, b) = edges[0];
-    return (cut_size, size[a as usize], size[b as usize]);
+    return edges;
+}
+
+fn karger_stein(
+    rng: &mut ThreadRng,
+    num_nodes: u16,
+    size: &mut [u32],
+    edges: &mut [(u16, u16)],
+) -> (usize, u32, u32) {
+    if num_nodes < 24 {
+        let edges = contract(rng, size, edges, num_nodes, 2);
+        let cut_size = edges.len();
+        let (a, b) = edges[0];
+        return (cut_size, size[a as usize], size[b as usize]);
+    } else {
+        let t = num_nodes * 2 / 3;
+
+        let (n1, a1, b1) = {
+            let mut size_copy_buffer = [0; MAX_NODES];
+            let size_copy = &mut size_copy_buffer[0..size.len()];
+            size_copy.copy_from_slice(size);
+            let mut edge_copy_buffer = [(0, 0); MAX_EDGES];
+            let edges_copy = &mut edge_copy_buffer[0..edges.len()];
+            edges_copy.copy_from_slice(edges);
+            let edges_copy = contract(rng, size_copy, edges_copy, num_nodes, t);
+            karger_stein(rng, t, size_copy, edges_copy)
+        };
+
+        let edges = contract(rng, size, edges, num_nodes, t);
+        let (n2, a2, b2) = karger_stein(rng, t, size, edges);
+
+        return if n1 < n2 { (n1, a1, b1) } else { (n2, a2, b2) };
+    }
 }
 
 fn main() {
     let mut edge_buffer = [(0, 0); MAX_EDGES];
     let (num_nodes, edges) = read_input(&mut edge_buffer);
     let mut rng = rand::thread_rng();
-    let mut runs = 0;
     loop {
-        runs += 1;
-        let (n, a, b) = karger(&mut rng, num_nodes, edges);
+        let mut size = [1; MAX_NODES];
+        let mut edge_copy_buffer = [(0, 0); MAX_EDGES];
+        let edges_copy = &mut edge_copy_buffer[0..edges.len()];
+        edges_copy.copy_from_slice(edges);
+        let (n, a, b) = karger_stein(&mut rng, num_nodes, &mut size, edges_copy);
         if n == 3 {
             print!("{}\n", a * b);
-            eprint!("Took {} runs\n", runs);
             break;
         }
     }
